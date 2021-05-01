0dd5d827a273e19afd67e6a67ac5f05bdba4d04c
Kenny Liao <yenming891109@berkeley.edu>
2021-05-01 14:23:28 +0800

    dundududd

--- solver.py
+++ solver.py
@@ -14,64 +14,88 @@ def solve(G):
         c: list of cities to remove
         k: list of edges to remove
     """
-    k, c = get_kc(len(G))
-    c_ret = remove_c(G)
-    k_ret = dijkstra_remove_k(G)
-    return c_ret, k_ret
-def remove_c(G):
-	ret = []
-	for i in range(c):
-		temp = 0
-		curr = 0
-		for j in G.node():
-			H = G.copy()
-	    	H.remove_node(j)
-			if nx.is_connected(H):
-				score = calculate_score(G, [j], [])
-				if score > curr:
-					temp = j
-					curr = score
-		G.remove_node(temp)
-		ret.append(temp)
-	return ret
-				
-def dijkstra_remove_k(G):
-	ret = []
-	for i in range(k):
-    	shortest = nx.dijkstra_path(G, 0, len(G) - 1)
-    	removed = False
-    	while removed is False:
-    		dame = []
-    		pos = max_weight_edge(shortest, G, dame)
-	    	H = G.copy()
-	    	H.remove_edge(shortest[pos], shortest[pos+1])
-	    	if nx.is_connected(H):
-	    		ret.append([shortest[pos], shortest[pos+1]])
-	    		G.remove_edge(shortest[pos], shortest[pos+1])
-	    		removed = True
-	    	else:
-	    		dame = [pos]
-	return ret
+    V = len(G)
+    (k, c) = get_kc(V)
+    c_ret = remove_c(G, c, V)
+    print(c_ret)
+    k_ret = dijkstra_remove_k(G, k, V)
+    print(c_ret)
+    print(k_ret)
+    return (c_ret, k_ret)
 
-def get_kc(G):
-	if len(G) <= 30:
-		return 15, 1
-	else if len(G) <= 50:
-		return 50, 3
-	else:
-		return 100, 5
+
+def remove_c(G, c, V):
+    ret = []
+    nodes = list(G.nodes())
+    nodes.remove(0)
+    nodes.remove(V-1)
+    for i in range(c):
+        temp = 0
+        curr = 0
+        for j in nodes:
+            H = G.copy()
+            H.remove_node(j)
+            if nx.is_connected(H):
+                score = calculate_score(G, [j], [])
+                if score > curr:
+                    temp = j
+                    curr = score
+        G.remove_node(temp)
+        ret.append(temp)
+    return ret
+
+
+def dijkstra_remove_k(G, k, V):
+    ret = []
+    for i in range(k):
+        shortest = nx.dijkstra_path(G, 0, V - 1)
+        removed = False
+        while removed is False:
+        	dame = []
+        	pos = max_weight_edge(shortest, G, dame)
+        	H = G.copy()
+        	H.remove_edge(shortest[pos], shortest[pos + 1])
+        	if nx.is_connected(H):
+        		ret.append([shortest[pos], shortest[pos + 1]])
+        		G.remove_edge(shortest[pos], shortest[pos + 1])
+        		removed = True
+        	else:
+        		dame = [pos]
+    return ret
+
+
+def get_kc(n):
+    if n <= 30:
+        return (15, 1)
+    elif n <= 50:
+        return (50, 3)
+    else:
+        return (100, 5)
+
 
 def max_weight_edge(p, G, no):
-	ret = 0
-	curr = 0
-	if len(no) != 0:
-		p = p[:no[0]]
-	for i in range(len(p) - 1):
-		temp = G[p[i]][p[i]+1]['weight']
-		if temp > curr:
-			curr = temp
-			ret = i
-	return ret
+    ret = 0
+    curr = 0
+    print(p)
+    if len(no) != 0:
+        p = p[:no[0]]
+    for i in range(len(p) - 1):
+        temp = G[p[i]][p[i] + 1]['weight']
+        if temp > curr:
+            curr = temp
+            ret = i
+    return ret
+
+
+if __name__ == '__main__':
+     assert len(sys.argv) == 2
+     path = sys.argv[1]
+     G = read_input_file(path)
+     c, k = solve(G)
+     assert is_valid_solution(G, c, k)
+     print("Shortest Path Difference: {}".format(calculate_score(G, c, k)))
+     write_output_file(G, c, k, 'outputs/ttest.out')
+
 
 # Here's an example of how to run your solver.
 
@@ -85,7 +109,6 @@ def max_weight_edge(p, G, no):
 #     assert is_valid_solution(G, c, k)
 #     print("Shortest Path Difference: {}".format(calculate_score(G, c, k)))
 #     write_output_file(G, c, k, 'outputs/small-1.out')
-
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 # if __name__ == '__main__':
